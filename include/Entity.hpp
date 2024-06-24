d1c57c73e0a9577ccdfa501a42d2500e94fb4838
Rohan_Tipnis <tipnis.rohan@gmail.com>
2024-06-24 12:10:18 -0500

    Movement System
    - Implemented the MovementSystem class that will handle the movement of entities
    - Fixed bug related to declaration of behaviors in the EntityManager
    - All type based functions are declared and defined in the Entity.hpp file
    - Added new behavior to check if an entity has a specific component or not.

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -57,9 +57,17 @@ namespace ember{
              * @param entity The entity to get the component from
              * @return The component associated with the entity
              */
-
             template<typename T>
             T& getComponent(Entity Entity);
+
+            /**
+             * @brief Checks if an entity has a specific component
+             * @tparam T the type of component
+             * @param entity The entity to check
+             * @return True if the entity has the component, false otherwise
+             */
+            template<typename T>
+            bool hasComponent(Entity entity);
 
         private:
             std::unordered_map<Entity, std::unordered_map<std::type_index, std::shared_ptr<void>>> entityComponents_;
@@ -68,5 +76,33 @@ namespace ember{
 
 
     };
+
+    template<typename T>
+    void EntityManager::addComponent(Entity entity, T component){
+        entityComponents_[entity][std::type_index(typeid(T))] = std::make_shared<T>(component);
+
+    }
+
+    template<typename T>
+    void EntityManager::removeComponent(Entity entity){
+        entityComponents_[entity].erase(std::type_index(typeid(T)));
+    }
+
+    template <typename T>
+    T& EntityManager::getComponent(Entity entity){
+        return *std::static_pointer_cast<T>(entityComponents_[entity][std::type_index(typeid(T))]);
+    }
+
+    template<typename T>
+    bool EntityManager::hasComponent(Entity entity){
+        auto entityIt = entityComponents_.find(entity);
+        if(entityIt != entityComponents_.end()){
+            auto componentIt = entityIt->second.find(std::type_index(typeid(T)));
+            if(componentIt != entityIt->second.end()){
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
--- src/EntityManager.cpp
+++ src/EntityManager.cpp
@@ -11,19 +11,4 @@ namespace ember{
         entityComponents_.erase(entity);
     }
 
-    template<typename T>
-    void EntityManager::addComponent(Entity entity, T component){
-        entityComponents_[entity][std::type_index(typeid(T))] = std::make_shared<T>(component);
-
-    }
-
-    template<typename T>
-    void EntityManager::removeComponent(Entity entity){
-        entityComponents_[entity].erase(std::type_index(typeid(T)));
-    }
-
-    template <typename T>
-    T& EntityManager::getComponent(Entity entity){
-        return *std::static_pointer_cast<T>(entityComponents_[entity][std::type_index(typeid(T))]);
-    }
 }// namespace ember
